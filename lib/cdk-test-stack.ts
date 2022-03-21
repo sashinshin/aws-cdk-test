eca3498da2685cbec42b3ad046ab4165981372f7
Sasha <sasha.luhr@gmail.com>
2022-03-21 15:36:43 +0100

    setup

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -3,11 +3,21 @@ import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName:'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('sashinshin/aws-cdk-test', 'main'),
+        commands: ['npm ci', 'npm run build', 'npm cdk synth'],
+      }),
+
+    })
 
     const lambda = new NodejsFunction(this, "lambda", {
       description: "Lambda",
@@ -20,7 +30,7 @@ export class CdkTestStack extends cdk.Stack {
       }
     });
 
-    
+
     // The code that defines your stack goes here
 
     // example resource
